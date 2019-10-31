5df139974f4487748de52a385bab92d720c71c84
ChristianSchill <christian.schill98@gmail.com>
2019-10-31 12:09:19 +0200

    Created the class BasicTank. Also implemented the the Getters for each attribute. Added a comment for GetMovement and GetBullet in class Tank to avoid confusion.

---
+++ BattleCity/BasicTank.cpp
@@ -0,0 +1,26 @@
+#include "BasicTank.h"
+
+std::string Basic_Tank::GetName() const
+{
+	return m_name;
+}
+
+uint16_t Basic_Tank::GetPoints() const
+{
+	return m_points;
+}
+
+uint8_t Basic_Tank::GetHealth() const
+{
+	return m_health;
+}
+
+uint8_t Basic_Tank::GetMovement() const
+{
+	return m_movement;
+}
+
+uint8_t Basic_Tank::GetBullet() const
+{
+	return m_bullet;
+}
--- BattleCity/Tank.h
+++ BattleCity/Tank.h
@@ -2,13 +2,14 @@
 #include <cstdint>
 #include <string>
 
-class tank
+class Tank
 {
 public :
 	virtual std::string GetName() const = 0;
 	virtual uint16_t GetPoints() const = 0;
 	virtual uint8_t GetHealth() const = 0;
-	virtual uint8_t GetMovement() const = 0;
-	virtual uint8_t GetBullet() const = 0;
+	virtual uint8_t GetMovement() const = 0; // returns the movement speed of the tank;
+	virtual uint8_t GetBullet() const = 0;  // returns the velocity of the projectile
+	
 };
 
