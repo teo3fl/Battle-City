00a9b60a81df7ed118c992174ab9719cacddda32
teo3fl <teo.rockgirl@yahoo.com>
2019-11-16 12:43:44 +0200

    Added SFML files: include and lib in the "External" folder, which will also contain other resources in the future. Added dll's.

--- BattleCity/Main.cpp
+++ BattleCity/Main.cpp
@@ -1,9 +1,8 @@
 #include <SFML/Graphics.hpp>
 
-
 int main()
 {
-	sf::RenderWindow window(sf::VideoMode(1280, 960), "SFML works!");
+	sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
 	sf::CircleShape shape(100.f);
 	shape.setFillColor(sf::Color::Green);
 
