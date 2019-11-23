8bb9d6b28eb1f8e33555415d45a9713d23f8b183
SpatarDiana <spatardiana@yahoo.ro>
2019-11-23 15:39:00 +0200

    Added class Enemy

---
+++ BattleCity/Enemy.cpp
@@ -0,0 +1,33 @@
+#include "Enemy.h"
+#include "TypesOfEnemy.h"
+
+Enemy::Enemy()
+{
+}
+
+Enemy::~Enemy()
+{
+}
+
+void Enemy::CreateEnemy()
+{
+}
+
+void Enemy::DestroyEnemy()
+{
+}
+
+void Enemy::SetEnemyType()
+{
+	
+}
+
+uint16_t Enemy::getId()
+{
+	return m_id;
+}
+uint32_t Enemy::getNumberOfEnemies()
+{
+	return m_numberOfEnemies;
+}
+
