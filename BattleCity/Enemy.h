de1d58e607f870655c7c614ee68b94ae4e62e5b6
SpatarDiana <spatardiana@yahoo.ro>
2019-11-23 15:34:24 +0200

    Added class Enemy

---
+++ BattleCity/Enemy.h
@@ -0,0 +1,27 @@
+#pragma once
+#include <string>
+#include <iostream>
+#include "TypesOfEnemy.h"
+#include <list>
+#include <vector>
+
+class Enemy
+{ 
+public:
+	Enemy();
+	~Enemy();
+	void CreateEnemy();
+	void DestroyEnemy();
+	void SetEnemyType();
+
+private:
+	uint32_t m_numberOfEnemies;
+	uint16_t m_id;
+	std::list<Enemy*> m_Enemies;         
+	std::vector<TypesOfEnemy> m_EnemiesType;  
+	
+		
+
+
+};
+
