b7f2f9ab668ed67688efad3519c426fbf8a625ce
teo3fl <teo.rockgirl@yahoo.com>
2019-12-22 21:49:37 +0200

    Implemented SFML functions in class Tile.

--- BattleCity/Tile.cpp
+++ BattleCity/Tile.cpp
@@ -1,1 +1,21 @@
 #include "Tile.h"
+
+Tile::Tile(const float x, const float y, const sf::Texture& texture)
+{
+	SetTexture(texture);
+}
+
+void Tile::SetTexture(const sf::Texture& texture)
+{
+	m_sprite.setTexture(texture);
+}
+
+void Tile::SetPosition(const float x, const float y)
+{
+	m_sprite.setPosition(x, y);
+}
+
+void Tile::Render(sf::RenderTarget* target)
+{
+	target->draw(m_sprite);
+}
