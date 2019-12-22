259f17846dc2cd60e2f2dedd0305a3d374ecc714
teo3fl <teo.rockgirl@yahoo.com>
2019-12-22 21:49:18 +0200

    Added SFML functions in class Tile.

--- BattleCity/Tile.h
+++ BattleCity/Tile.h
@@ -1,10 +1,21 @@
 #pragma once
 #include <string>
 
+#include "Entity.h"
+
 class Tile
 {
 public:
+	Tile(const float x, const float y, const sf::Texture& texture);
+	void SetTexture(const sf::Texture& texture);
+	void SetPosition(const float x, const float y);
+	virtual void Update(const float& dt);
+	void Render(sf::RenderTarget* target);
+
 	virtual const std::string GetType() const = 0;
 	virtual bool IsPassable() const = 0;
 	virtual bool IsBreakable() const = 0;
+
+private:
+	sf::Sprite m_sprite;
 };
