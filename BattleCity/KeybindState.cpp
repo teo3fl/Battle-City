e6973a3b11cc6272301fb1a31b7159b16e4e9ac6
teo3fl <teo.rockgirl@yahoo.com>
2020-01-01 18:12:50 +0200

    Modified KeybindState so that it reads both the actions and the keys from a file.

--- BattleCity/KeybindState.cpp
+++ BattleCity/KeybindState.cpp
@@ -20,34 +20,53 @@ void KeybindState::InitializeBackground()
 
 void KeybindState::InitializeText(sf::Color color, uint8_t size)
 {
-	std::stringstream ss;
+	std::stringstream ss_keys;
+	std::stringstream ss_actions;
 	
-	ss <<
-		"Move up\n" <<
-		"Move down\n" <<
-		"Move left\n" <<
-		"Move Right\n" <<
-		"Shoot\n" <<
-		"Back to Main Meniu\n";
+	std::ifstream in("../External/Resources/Config/gamestate_keybinds.ini");
+
+	if (in.is_open())
+	{
+		std::string action = "";
+		std::string key = "";
+
+		while (in >> action >> key)
+		{
+			ss_actions << action << std::endl;
+			ss_keys << key << std::endl;
+		}
+	}
+	else
+		throw "ERROR::KEYBINDS_STATE::KEYBINDS_NOT_FOUND";
+
+	in.close();
+
+
+	//ss_actions <<
+	//	"Move up\n" <<
+	//	"Move down\n" <<
+	//	"Move left\n" <<
+	//	"Move Right\n" <<
+	//	"Shoot\n" <<
+	//	"Back to Main Meniu\n";
 
 	m_actions.setFont(*m_font);
-	m_actions.setString(ss.str());
+	m_actions.setString(ss_actions.str());
 	m_actions.setFillColor(color);
 	m_actions.setCharacterSize(size);
 	m_actions.setPosition(20, 20);
-		
-	ss.str(std::string());
 
-	ss <<
-		"W\n" <<
-		"S\n" <<
-		"A\n" <<
-		"D\n" <<
-		"???\n" <<
-		"Escape\n";
+
+	//ss_keys <<
+	//	"W\n" <<
+	//	"S\n" <<
+	//	"A\n" <<
+	//	"D\n" <<
+	//	"???\n" <<
+	//	"Escape\n";
 
 	m_keybinds.setFont(*m_font);
-	m_keybinds.setString(ss.str());
+	m_keybinds.setString(ss_keys.str());
 	m_keybinds.setFillColor(color);
 	m_keybinds.setCharacterSize(size);
 	m_keybinds.setPosition(500, 20);
