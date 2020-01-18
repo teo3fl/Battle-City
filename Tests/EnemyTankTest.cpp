9546a31a9d251d6c59208c84adcc8143f9cb0fcf
teo3fl <teo.rockgirl@yahoo.com>
2020-01-18 10:55:03 +0200

    Added 4 EnemyTank tests.

---
+++ Tests/EnemyTankTest.cpp
@@ -0,0 +1,40 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+#include "ArmorTank.h"
+#include "BasicTank.h"
+#include "FastTank.h"
+#include "PowerTank.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace Tests
+{
+	TEST_CLASS(EnemyTankTest)
+	{
+	public:
+		TEST_METHOD(ArmorTankHealth)
+		{
+			ArmorTank armorTank;
+			Assert::IsTrue(armorTank.GetHealth()==4);
+		}
+
+		TEST_METHOD(BasicTankType)
+		{
+			BasicTank basicTank;
+			Assert::IsTrue(basicTank.GetType() == "BasicTank");
+		}
+
+		TEST_METHOD(FastTankSpeed)
+		{
+			FastTank fastTank;
+			Assert::IsTrue(fastTank.GetMovement() == 150.f);
+		}
+
+		TEST_METHOD(PowerTankPoints)
+		{
+			PowerTank powerTank;
+			Assert::IsTrue(powerTank.GetPoints() == 300);
+		}
+
+	};
+}
