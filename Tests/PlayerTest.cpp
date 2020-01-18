6ce085a90173bf44c23a9b825fc1aa459f11be66
teo3fl <teo.rockgirl@yahoo.com>
2020-01-18 10:33:11 +0200

    Added 7 Player tests.

--- Tests/PlayerTest.cpp
+++ Tests/PlayerTest.cpp
@@ -10,7 +10,7 @@ namespace Tests
 	{
 	public:
 
-		TEST_METHOD(TestMethod3)
+		TEST_METHOD(Protected)
 		{
 			Player player("Player",0,0);
 			player.IsProtected();
@@ -18,7 +18,53 @@ namespace Tests
 				Assert::IsTrue;
 			else
 				Assert::IsFalse;
+		}
 
+		TEST_METHOD(Position)
+		{
+			Player player("Player", 345, 123);
+			Assert::IsTrue(player.GetPosition() == sf::Vector2f(345, 123));
+		}
+
+		TEST_METHOD(Bullet)
+		{
+			Player player("Player", 0, 0);
+			player.Fire();
+			Assert::IsTrue(player.GetBullet() != nullptr);
+		}
+
+		TEST_METHOD(Score)
+		{
+			Player player("Player", 0, 0);
+			player.IncreaseScore(500);
+			Assert::IsTrue(player.GetScore() ==500);
+		}
+
+		TEST_METHOD(SecondaryBullet)
+		{
+			Player player("Player", 0, 0);
+			Assert::IsTrue(player.CanFireSecondaryBullet() == false);
+		}
+
+		TEST_METHOD(SecondaryBullet2)
+		{
+			Player player("Player", 0, 0);
+			player.IncreaseOffensivePower();
+			player.IncreaseOffensivePower();
+			Assert::IsTrue(player.CanFireSecondaryBullet() == true);
+		}
+
+		TEST_METHOD(Lives)
+		{
+			Player player("Player", 0, 0);
+			Assert::IsTrue(player.GetLives() == 1);
+		}
+
+		TEST_METHOD(Lives2)
+		{
+			Player player("Player", 0, 0);
+			player.GiveExtraLife();
+			Assert::IsTrue(player.GetLives() == 2);
 		}
 	};
 }
